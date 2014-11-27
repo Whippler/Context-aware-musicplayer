5394aafd065dd7b14ceef8c847536df14bae6320
Mikko Pelkonen <apelkone@cs.helsinki.fi>
2014-11-27 16:07:50 +0200

    Preliminary activity tags when loading files

--- MusicPlayer/src/com/androidhive/musicplayer/SongsManager.java
+++ MusicPlayer/src/com/androidhive/musicplayer/SongsManager.java
@@ -9,7 +9,7 @@ import android.os.Environment;
 
 public class SongsManager {
 	// SDCard Path
-	final String MEDIA_PATH = new String("/storage/");
+	final String MEDIA_PATH = new String("/storage/sdcard0/");
 	final static String MEDIA_PATH2 = new String("/storage/SD card/Media/Music/Arch Enemy");
 	private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
 	
@@ -35,7 +35,7 @@ public class SongsManager {
             // return songs list array
             return songsList;
 	}
-	
+
 	private String getMusicDirectory(File root){
 		
 		String retPath = "";
@@ -48,27 +48,44 @@ public class SongsManager {
 	
         private ArrayList<HashMap<String, String>> find_files(File root)
         {
-        	try{
-	            File[] files = root.listFiles();
-	            for (File file : files) {
-	                if (file.isFile()) {
-	                    if(file.getName().endsWith(".mp3") || file.getName().endsWith(".MP3")) {
-	                        HashMap<String, String> song = new HashMap<String, String>();
-	                        song.put("songTitle", file.getName().substring(0, (file.getName().length() - 4)));
-	                        song.put("songPath", file.getAbsolutePath());
-	                        
-	                        // Adding each song to SongList
-	                        songsList.add(song);
-	                    }
-	                } else if (file.isDirectory()) {
-	                    find_files(file);
-	                }
-	            }
-	            return songsList;
-        	}
-        	catch(Exception e){
-        		return null;
-        	}
+            try{
+                File[] files = root.listFiles(); 
+                for (File file : files) {
+                    if (file.isFile()) {
+                        if(file.getName().endsWith(".mp3") || file.getName().endsWith(".MP3")) {
+                            HashMap<String, String> song = new HashMap<String, String>();
+                            song.put("songTitle", file.getName().substring(0, (file.getName().length() - 4)));
+                            song.put("songPath", file.getAbsolutePath());
+                            if(file.getAbsolutepath.matches("(.*)\\/running\\/(.*)")) {
+                                song.put("activity", "running");
+                            }
+                            else if(file.getAbsolutepath.matches("(.*)\\/walking\\/(.*)")) {
+                                song.put("activity", "walking");
+                            }
+                            else if(file.getAbsolutepath.matches("(.*)\\/still\\/(.*)")) {
+                                song.put("activity", "still");
+                            }
+                            else if(file.getAbsolutepath.matches("(.*)\\/vehicle\\/(.*)")) {
+                                song.put("activity", "vehicle");
+                            }
+                            else if(file.getAbsolutepath.matches("(.*)\\/cycling\\/(.*)")) {
+                                song.put("activity", "cycling");
+                            }
+                            else {
+                                song.put("activity", "default");
+                            }
+                            // Adding each song to SongList
+                            songsList.add(song);
+                        }
+                    } else if (file.isDirectory()) {
+                        find_files(file);
+                    }
+                }
+            }
+            catch(Exception e){
+                return null;
+            }
+            return songsList;
         }
 	
 	/**
