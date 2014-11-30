73dba92959ed51d8ddc18b634a385ae766255ea5
Angel Gallegos <angel.gallegosx@gmail.com>
2014-11-30 05:43:50 +0200

    "Cleaner" code and now it receives messages from both aware systems.

---
+++ ContextAwareSystem/src/com/example/contextawaresystem/ActivityRecognitionService.java
@@ -0,0 +1,111 @@
+package com.example.contextawaresystem;
+
+import org.json.JSONObject;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.util.Log;
+
+public class ActivityRecognitionService extends IntentService{
+
+	public static final String prefKeyType = "PREVIOUS_ACTIVITY";
+	private SharedPreferences mPrefs;
+	
+	public ActivityRecognitionService() {
+		super("Whatever");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+		mPrefs = getApplicationContext().getSharedPreferences(RecognitionUtilities.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+		
+		if(ActivityRecognitionResult.hasResult(intent)){
+			// Get the update
+			ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+			Log.i("Service", getType(result.getMostProbableActivity().getType()) +"\t" + result.getMostProbableActivity().getConfidence());
+			
+			// Get the most probable activity from the list of activities in the update
+            DetectedActivity mostProbableActivity = result.getMostProbableActivity();
+            
+            int activityType = mostProbableActivity.getType();
+            
+            //If there's no activity yet or the new one is greater than the old one
+            if (!mPrefs.contains(prefKeyType)){
+            	storeActivity(activityType);
+            	RecognitionUtilities.sendMessage(this, toJson(mostProbableActivity).toString());
+            }
+            else if (activityChanged(activityType) && mostProbableActivity.getConfidence() > 50){
+            	storeActivity(activityType);
+            	RecognitionUtilities.sendMessage(this, toJson(mostProbableActivity).toString());
+            }
+            
+		}
+		
+	}
+	
+	/**Detects if the new values is different form the last activity */
+	private boolean activityChanged(int currentType) {
+		
+		if(mPrefs.getInt(prefKeyType, DetectedActivity.UNKNOWN) != currentType){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/** Store the last activity value */
+	public void storeActivity(int activityType){
+
+		Editor editor = mPrefs.edit();
+   	 	editor.putInt(prefKeyType, activityType);
+   	 
+   	 	editor.commit();
+   	 	
+	}
+	
+	private String getType(int type){
+		if(type == DetectedActivity.UNKNOWN)
+			return "Unknown";
+		else if(type == DetectedActivity.IN_VEHICLE)
+			return "Vehicle";
+		else if(type == DetectedActivity.ON_BICYCLE)
+			return "Bicycle";
+		else if(type == DetectedActivity.ON_FOOT)
+			return "Foot";
+		else if(type == DetectedActivity.STILL)
+			return "Still";
+		else if(type == DetectedActivity.TILTING)
+			return "Tilting";
+		else
+			return "";
+	}
+	
+	private JSONObject toJson(DetectedActivity mostProbableActivity){
+		JSONObject retVal = new JSONObject();
+		try {
+			retVal.put("MessageType", "ActivityRecognition");
+			retVal.put("ActivityID", mostProbableActivity.getType());
+			retVal.put("ActivityName", getType(mostProbableActivity.getType()));
+			retVal.put("ActivityConfidence", mostProbableActivity.getConfidence());
+			return retVal;
+		    
+		}
+		catch(Exception e){
+			Log.e("ActivityRecognitionService", "Error trying to parse to Json");
+			  
+			return null;
+		}
+		
+	}
+
+}
---
+++ ContextAwareSystem/src/com/example/contextawaresystem/LocationDriver.java
@@ -0,0 +1,160 @@
+package com.example.contextawaresystem;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
+public class LocationDriver implements LocationListener, GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener{
+
+	private Context context;
+	// A request to connect to Location Services
+    private LocationRequest mLocationRequest;
+    // Stores the current instantiation of the location client in this object
+    private LocationClient mLocationClient;
+    
+    public LocationDriver(Context context){
+    	this.context = context;
+    	
+    	mLocationRequest = LocationRequest.create(); 
+    	mLocationRequest.setInterval(RecognitionUtilities.LOCATION_UPDATE_INTERVAL_IN_MILISECONDS); //update interval
+    	mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //Accuracy level
+    	mLocationRequest.setFastestInterval(RecognitionUtilities.LOCATION_FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+    	mLocationClient = new LocationClient(context, this, this); //Create a location request with default parameters.
+    }
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		startPeriodicUpdates();
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		stopPeriodicUpdates();
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		JSONObject temp = getLocality();
+		if(temp!=null){
+			RecognitionUtilities.sendMessage(context, temp.toString());
+		}
+	}
+	
+	/** Connects the client to Google Play services */
+	public void connect(){
+    	mLocationClient.connect();
+    }
+	
+	public boolean isConnected(){
+    	return mLocationClient.isConnected();
+    }
+	
+	/** Disconnects the client to Google Play services */
+	public void disconnect(){
+    	mLocationClient.disconnect();
+    }
+	
+	/** Get the actual location */
+	private Location getLocation(){
+	    	Location currentLocation = null;
+	    	
+	    	if(RecognitionUtilities.GoogleServicesValidator(context)){
+	    		currentLocation = mLocationClient.getLastLocation();
+	    	}
+	    	else{
+	    		Toast.makeText(context, "Please install Google Play Service.", Toast.LENGTH_SHORT).show();
+		}
+		
+		return currentLocation;
+	}
+	
+	/** Get the address from the location */
+	public JSONObject getLocality(){
+    	
+		JSONObject retVal = null;
+		Geocoder geocoder= new Geocoder(context, Locale.ENGLISH);
+		
+		try{
+			Location location = getLocation();
+			Log.i("getAdress", location.getLatitude()+"");
+				
+			List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+		   	 
+			if(addresses != null) {
+					  
+					// get the address
+					Address fetchedAddress = addresses.get(0); 
+					retVal = new JSONObject();
+					
+					try {
+						retVal.put("MessageType", "Location");
+						retVal.put("Country", fetchedAddress.getCountryName());
+						retVal.put("Code", fetchedAddress.getCountryCode());
+						retVal.put("City", fetchedAddress.getSubAdminArea());
+						retVal.put("Locality", fetchedAddress.getLocality());
+						retVal.put("Coordinates", fetchedAddress.getLatitude()+","+fetchedAddress.getLongitude());
+					}
+					catch(Exception e){
+						
+						return null;
+					}				
+					
+			}
+		  
+			return retVal;
+		  
+		}
+    	catch(Exception e){
+    		
+    		return null;
+    		/*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD && !Geocoder.isPresent()) {
+                return null;
+            }
+    		else
+    			return e.getLocalizedMessage();*/
+    	}
+    }
+
+	/** Requests location updates */
+	public void startPeriodicUpdates() {
+    	if(RecognitionUtilities.GoogleServicesValidator(context)){
+    		mLocationClient.requestLocationUpdates(mLocationRequest, this); //From here it is possible to create an intent
+    	}
+    }
+    
+    public void stopPeriodicUpdates() {
+    	if(mLocationClient!=null){
+    		mLocationClient.removeLocationUpdates(this);
+    	}
+    }
+
+}
